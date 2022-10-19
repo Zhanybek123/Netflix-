dcdc224cd611ae2fbd2a68602180485626d3b901
zhany <zhanybeksalgarin@gmail.com>
2022-10-19 09:47:44 -0400

    Initial Commit

---
+++ MyNetflix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyNetflix
+//
+//  Created by zhanybek salgarin on 10/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
